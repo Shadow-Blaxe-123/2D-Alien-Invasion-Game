6dc400142f58bc7b75725739670560be6284dc0e
Shadow-Blaxe-123 <shamunahmed38@gmail.com>
2026-07-16 23:39:49 +0600

    Initial commit

---
+++ game.py
@@ -0,0 +1,12 @@
+class Game:
+    def __init__(self):
+        pass
+    # Main Game loop
+    def run_game(self):
+        pass
+    def exit(self):
+        pass
+    def check_input(self):
+        pass
+    def update_screen(self):
+        pass
---
+++ ship.py
@@ -0,0 +1,7 @@
+class Ship:
+    def __init__(self):
+        pass
+    def update_position(self):
+        pass
+    def draw(self):
+        pass
