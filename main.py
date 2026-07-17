9d0b3c525efa7e35ae5478c02c092546f42afdf3
Shadow-Blaxe-123 <shamunahmed38@gmail.com>
2026-07-17 23:07:00 +0600

    Background image done.

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import sys
 import pygame
 
 from settings import Settings
+from menu import Menu
 
 if __name__ == "__main__":
     pygame.init()
     running = True
     settings = Settings()
     screen = pygame.display.set_mode(settings.screen_size)
+    menu = Menu(screen)
     pygame.display.set_caption("2D Alien Invasion Game")
 
     while running:
@@ -16,5 +18,4 @@ if __name__ == "__main__":
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-        screen.fill("white")
-        pygame.display.update()
+        menu.display()
--- menu.py
+++ menu.py
@@ -1,2 +1,14 @@
+import pygame
+from pygame import SurfaceType
+
+
+
 class Menu:
-    pass
+    def __init__(self, screen: SurfaceType):
+        self.screen = screen
+        self.background = pygame.image.load("assets/Background.png")
+        self.background = pygame.transform.scale(self.background, self.screen.get_size())
+
+    def display(self):
+        self.screen.blit(self.background, (0,0) )
+        pygame.display.flip()
