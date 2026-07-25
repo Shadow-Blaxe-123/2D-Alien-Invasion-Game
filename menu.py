2e2dd24d89d50b996d4d4da2aa5fad64569881b5
Shadow-Blaxe-123 <shamunahmed38@gmail.com>
2026-07-26 00:03:54 +0600

    Got the play button finally

--- menu.py
+++ menu.py
@@ -7,11 +7,20 @@ class Menu:
     def __init__(self, screen: SurfaceType):
         self.screen = screen
         self.screen_center = (screen.get_width()//2, screen.get_height()//2)
+        self.text = "Play"
 
 
     def display(self):
         # TODO: ADD Content
-        pygame.display.flip()
-        pygame.draw.rect(self.screen, (255,255,255), (640,360,25,25),5)
 
+        font = pygame.font.SysFont("sans-serif",100)
+        text_render = font.render(self.text,True, "green", "blue")
+
+        text_rect = text_render.get_rect()
+
+        # set the center of the rectangular object.
+        text_rect.center = self.screen_center
+
+        self.screen.blit(text_render, text_rect)
         pygame.display.flip()
+
